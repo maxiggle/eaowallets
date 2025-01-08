2d77114a6b4cfc2b71a9ac0f9f29ecbb69657233
Godwin Alexander Ekainu <clsyfriday@gmail.com>
2025-01-08 17:23:58 +0100

    expose methods

--- lib/eoawallet.dart
+++ lib/eoawallet.dart
@@ -1,3 +1,5 @@
 export 'package:eoawallet/eoa_wallet_manager.dart';
 export 'package:eoawallet/configuration/rpc_base_config.dart';
 export 'package:eoawallet/helper/eoa_wallet_helpers.dart';
+export 'package:eoawallet/constant/abis/contract_abis.dart';
+export 'token_service.dart';
